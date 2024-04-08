ceb8d0479647a410decdcfd5e3c69ae710d94d41
C Yongky Pranowo <cokroyongkyp@gmail.com>
2024-04-08 23:08:54 +0700

    Tes Deploy to server

--- database/database.go
+++ database/database.go
@@ -3,17 +3,17 @@ package database
 import (
 	"fmt"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func DatabaseInit() {
-	// const MYSQL = "root:@tcp(127.0.0.1:3306)/go_fiber_gorm?charset=utf8mb4&parseTime=True&loc=Local"
-	const POSTGRES = "user=postgres password=postgres dbname=go_fiber_gorm port=5432 sslmode=disable TimeZone=Asia/Jakarta"
-	dsn := POSTGRES
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	const MYSQL = "mr_cyp:L0rem!psum'@tcp(127.0.0.1:3306)/go_fiber_gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	// const POSTGRES = "user=postgres password=postgres dbname=go_fiber_gorm port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := MYSQL
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}

cb8a7d30110ed7d4737b1ccb3036446a405eb834
C Yongky Pranowo <cokroyongkyp@gmail.com>
2024-04-08 23:15:39 +0700

    test remove user

--- database/database.go
+++ database/database.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func DatabaseInit() {
-	const MYSQL = "mr_cyp:L0rem!psum'@tcp(127.0.0.1:3306)/go_fiber_gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	const MYSQL = "mr_cyp:L0rem!psum@tcp(127.0.0.1:3306)/go_fiber_gorm?charset=utf8mb4&parseTime=True&loc=Local"
 	// const POSTGRES = "user=postgres password=postgres dbname=go_fiber_gorm port=5432 sslmode=disable TimeZone=Asia/Jakarta"
 	dsn := MYSQL
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
